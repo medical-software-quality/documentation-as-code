8a342f50949e1cb92c7f0085d77474ac0aa8fdfe
Jorge C. Leitao <jorgecarleitao@gmail.com>
2023-01-02 11:14:51 +0000

    More

--- tests/main.rs
+++ tests/main.rs
@@ -2,44 +2,34 @@ use cucumber::{given, then, when, World as _};
 
 use assert_cmd::Command;
 
-#[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("quality")?;
-
-    cmd.arg("path").arg("test/file/doesnt/exist");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("could not read file"));
-
-    Ok(())
-}
-
 #[derive(cucumber::World, Debug, Default)]
 struct World {
     path: String,
+    command: Option<Command>,
 }
 
-#[given(expr = "a solution")]
+#[given(expr = "a solution in a directory without any documentation")]
 fn a_solution(w: &mut World) {
-    w.path = "./tests/project1".to_string();
+    w.path = "./tests/fixtures/not_a_directory".to_string();
 }
 
 #[when(expr = "its documentation is checked")]
 fn check_docs(w: &mut World) {
     let mut cmd = Command::cargo_bin("quality").unwrap();
 
-    cmd.arg("path").arg("test/file/doesnt/exist");
+    cmd.arg("--path").arg(&w.path);
 
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("could not read file"));
-
-    assert!(w.capacity < 4, "{} exploded!", w.user.as_ref().unwrap());
+    w.command = Some(cmd);
 }
 
-#[then("she is full")]
-fn is_full(w: &mut World) {
-    assert_eq!(w.capacity, 3, "{} isn't full!", w.user.as_ref().unwrap());
+#[then("the check fails")]
+fn check_fails(w: &mut World) {
+    let command = std::mem::take(&mut w.command);
+    command
+        .unwrap()
+        .assert()
+        .failure()
+        .stdout(predicates::str::contains("ERROR"));
 }
 
 fn main() {
