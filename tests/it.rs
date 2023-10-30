2c1da76633d42463aab19937815669256ef71782
Jorge Leitao <jorgecarleitao@gmail.com>
2023-10-30 04:42:08 +0100

    Improved tests

--- tests/it.rs
+++ tests/it.rs
@@ -1,7 +1,17 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::path::{Path, PathBuf};
+
 use assert_cmd::Command;
 use cucumber::{given, then, when, World as _};
 use gherkin::Step;
 use predicates::prelude::PredicateBooleanExt;
+
+fn dir_name<'a>(iter: impl Iterator<Item = &'a str>) -> String {
+    let mut hasher = DefaultHasher::new();
+    iter.for_each(|item| item.hash(&mut hasher));
+    format!("{}", hasher.finish())
+}
 
 fn create_local_project(
     spec: &str,
@@ -11,9 +21,20 @@ fn create_local_project(
     user_manual: &str,
     operator_manual: &str,
     retirement_plan: &str,
-) -> std::path::PathBuf {
+) -> PathBuf {
     let dir = std::env::temp_dir();
-    let dir = dir.join("project");
+    let dir = dir.join(dir_name(
+        [
+            spec,
+            design,
+            risk,
+            test,
+            user_manual,
+            operator_manual,
+            retirement_plan,
+        ]
+        .into_iter(),
+    ));
     let _ = std::fs::remove_dir_all(&dir);
     let _ = std::fs::create_dir(&dir);
     let _ = std::fs::create_dir(dir.join("features"));
@@ -58,6 +79,12 @@ fn create_local_project(
     dir
 }
 
+fn command(path: &Path) -> Command {
+    let mut cmd = Command::cargo_bin("quality").unwrap();
+    cmd.arg("--path").arg(path);
+    cmd
+}
+
 #[derive(cucumber::World, Debug, Default)]
 struct World {
     feature: String,
@@ -68,7 +95,7 @@ struct World {
     operator_manual: String,
     retirement_plan: String,
     has_spec: bool,
-    command: Option<Command>,
+    path: PathBuf,
 }
 
 #[given(expr = "software without a specification")]
@@ -122,7 +149,7 @@ fn a_retirement(w: &mut World, step: &Step) {
 #[when(expr = "we check its documentation")]
 #[when(expr = "we check it")]
 fn check_docs(w: &mut World) {
-    let path = if w.has_spec {
+    w.path = if w.has_spec {
         create_local_project(
             &w.feature,
             &w.design_specification,
@@ -135,26 +162,18 @@ fn check_docs(w: &mut World) {
     } else {
         "./not_a_directory".into()
     };
-
-    let mut cmd = Command::cargo_bin("quality").unwrap();
-
-    cmd.arg("--path").arg(&path);
-
-    w.command = Some(cmd);
 }
 
 #[then("we get an error of a missing risk assessment file")]
 fn missing_risk(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command.unwrap().assert().failure().stdout(
+    command(&w.path).assert().failure().stdout(
         predicates::str::contains("ERROR").and(predicates::str::contains("risk_assessment.md")),
     );
 }
 
 #[then("we get an error of a missing design specification")]
 fn missing_design(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command.unwrap().assert().failure().stdout(
+    command(&w.path).assert().failure().stdout(
         predicates::str::contains("ERROR")
             .and(predicates::str::contains("design_specification.md")),
     );
@@ -162,41 +181,35 @@ fn missing_design(w: &mut World) {
 
 #[then("we get an error of a missing retirement plan")]
 fn missing_retirement(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command.unwrap().assert().failure().stdout(
+    command(&w.path).assert().failure().stdout(
         predicates::str::contains("ERROR").and(predicates::str::contains("retirement_plan.md")),
     );
 }
 
 #[then("we get an error of a missing verification plan")]
 fn missing_verification(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command.unwrap().assert().failure().stdout(
+    command(&w.path).assert().failure().stdout(
         predicates::str::contains("ERROR").and(predicates::str::contains("verification_plan.md")),
     );
 }
 
 #[then("we get an error of a missing user manual file")]
 fn missing_user_manual(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command.unwrap().assert().failure().stdout(
+    command(&w.path).assert().failure().stdout(
         predicates::str::contains("ERROR").and(predicates::str::contains("user_manual.md")),
     );
 }
 
 #[then("we get an error of a missing operator manual file")]
 fn missing_operator_manual(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command.unwrap().assert().failure().stdout(
+    command(&w.path).assert().failure().stdout(
         predicates::str::contains("ERROR").and(predicates::str::contains("operator_manual.md")),
     );
 }
 
 #[then("we get an error of an incorrect risk assessment")]
 fn check_fails_identifier_risk(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -208,9 +221,7 @@ fn check_fails_identifier_risk(w: &mut World) {
 
 #[then("we get an error of an incorrect design specification")]
 fn check_fails_identifier_design(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -222,9 +233,7 @@ fn check_fails_identifier_design(w: &mut World) {
 
 #[then("we get an error of an incorrect header in design specification")]
 fn then_missing_header_in_design(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -236,9 +245,7 @@ fn then_missing_header_in_design(w: &mut World) {
 
 #[then("we get an error of an incorrect verification plan")]
 fn check_fails_identifier_verification(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -250,9 +257,7 @@ fn check_fails_identifier_verification(w: &mut World) {
 
 #[then("we get an error of an incorrect user manual")]
 fn check_fails_identifier_user_manual(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -264,9 +269,7 @@ fn check_fails_identifier_user_manual(w: &mut World) {
 
 #[then("we get an error of an incorrect operator manual")]
 fn check_fails_identifier_operator_manual(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -278,9 +281,7 @@ fn check_fails_identifier_operator_manual(w: &mut World) {
 
 #[then("we get an error of an incorrect retirement plan")]
 fn check_fails_identifier_retirement_plan(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -292,9 +293,7 @@ fn check_fails_identifier_retirement_plan(w: &mut World) {
 
 #[then("we get an error of a missing specification")]
 fn check_fails_specification(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(predicates::str::contains("/features"));
@@ -302,9 +301,7 @@ fn check_fails_specification(w: &mut World) {
 
 #[then("we get an error regarding a wrong identifier")]
 fn check_fails_identifier(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -316,9 +313,7 @@ fn check_fails_identifier(w: &mut World) {
 
 #[then("we get an error regarding a wrong trace in risks")]
 fn check_fails_identifier_trace_risk(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -330,9 +325,7 @@ fn check_fails_identifier_trace_risk(w: &mut World) {
 
 #[then("we get an error regarding a wrong trace in design")]
 fn check_fails_identifier_trace_design(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -344,9 +337,7 @@ fn check_fails_identifier_trace_design(w: &mut World) {
 
 #[then("we get an error regarding a wrong trace in verification plan")]
 fn check_fails_identifier_trace_verification(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -358,9 +349,7 @@ fn check_fails_identifier_trace_verification(w: &mut World) {
 
 #[then("we get an error regarding a wrong trace in user manual")]
 fn check_fails_identifier_trace_manual(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(
@@ -372,9 +361,7 @@ fn check_fails_identifier_trace_manual(w: &mut World) {
 
 #[then("we get an error regarding wrong Gherkin")]
 fn check_fails_gherkin(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command
-        .unwrap()
+    command(&w.path)
         .assert()
         .failure()
         .stdout(predicates::str::contains("ERROR"));
@@ -382,14 +369,12 @@ fn check_fails_gherkin(w: &mut World) {
 
 #[then("we get no error")]
 fn check_ok(w: &mut World) {
-    let command = std::mem::take(&mut w.command);
-    command.unwrap().assert().success();
+    command(&w.path).assert().success();
 }
 
 #[then("we get the following JSON")]
 fn check_json(w: &mut World, step: &Step) {
-    let command = std::mem::take(&mut w.command);
-    let assert = command.unwrap().assert().success();
+    let assert = command(&w.path).assert().success();
     let output = assert.get_output();
     assert_eq!(
         serde_json::from_slice::<serde_json::Value>(&output.stdout).unwrap(),
